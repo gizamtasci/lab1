62a81ac8addb33b87b79023d45bdb847b40d1e03
gizamtasci <50792712+gizamtasci@users.noreply.github.com>
2019-10-09 14:03:01 +0300

    Create Dosyalama.h

---
+++ Dosyalama.h
@@ -0,0 +1,24 @@
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class DosyaIslem
+{
+public:
+	vector<int> dizi;
+	/**
+* \Dosyadan okuma islemi yapar
+* 
+*/
+	void Okuma();
+	/**
+* \vector yapısındaki diziyi dondurur
+* \return dizi
+*/
+	vector<int> DiziDondur();
+
+private:
+
+};
+
